76c6de4808896b4f420864cf1bd2b7a1e4baf32c
Thiago <thiagllima26@gmail.com>
2025-10-01 23:46:49 -0300

    projeto atualizado

--- src/main/java/com/example/mottu_springboot/MottuApplication.java
+++ src/main/java/com/example/mottu_springboot/MottuApplication.java
@@ -2,12 +2,12 @@ package com.example.mottu_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MottuSpringbootApplication {
-
+public class MottuApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MottuSpringbootApplication.class, args);
+		SpringApplication.run(MottuApplication.class, args);
 	}
-
 }
